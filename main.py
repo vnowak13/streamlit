cbc477520e629d4cd7c9f219d920a91f1698b247
Nowak <vnowak@cps.k12.il.us>
2024-12-10 12:48:57 -0600

    added new stuff

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ st.write("Hello, World!")
 st.title("Title")
 st.header("Header")
 st.subheader("Sub-header")
+
+
+st.title("Simple Web Form with Streamlit")
+st.header("User Information Users")
+name = st.text_input("Enter your name: ")
+options = ["Option 1", "Option 2", "Option 3"]
+selected_option = st.selectbox("Choose an option: ", options)
