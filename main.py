0f58758ed840ceb42114e162f53b7fa1ed064b6a
Nowak <vnowak@cps.k12.il.us>
2024-12-16 11:33:50 -0600

    Added new stuff

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import streamlit as st
-st.write("Hello, World!")
 
-st.title("Title")
-st.header("Header")
-st.subheader("Sub-header")
-
-
+# Set the title of the web page
 st.title("Simple Web Form with Streamlit")
+
+# Set a header
 st.header("User Information Users")
+# Text input for name
 name = st.text_input("Enter your name: ")
+# Dropdown menu for selecting an option
 options = ["Option 1", "Option 2", "Option 3"]
-selected_option = st.selectbox("Choose an option: ", options)
+selected_option = st.selectbox("Choose an Option:", options)
+# Slider for selecting a value 
+
+# Submit button
+if st.button("Sumbit"):
+    st.write(f"Name; {name}")
+    st.write(f"Selected Option: {selected_option}")
+# Additional information
+
+# Footer
+
