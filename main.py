924b30f7a46f65ece9f8ae186565a8731d7ac880
Nowak <vnowak@cps.k12.il.us>
2024-12-16 11:47:50 -0600

    added new stuff

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ name = st.text_input("Enter your name: ")
 options = ["Option 1", "Option 2", "Option 3"]
 selected_option = st.selectbox("Choose an Option:", options)
 # Slider for selecting a value 
-
+slider_value = st.slider("Select as value", 1, 100, 50)
 # Submit button
 if st.button("Sumbit"):
-    st.write(f"Name; {name}")
+    st.write(f"Name: {name}")
     st.write(f"Selected Option: {selected_option}")
 # Additional information
-
+st.subheader("Summary")
+st.write("Fill out the form above and click the Sumbit button")
 # Footer
 
